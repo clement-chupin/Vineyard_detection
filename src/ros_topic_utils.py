adbbcf3eff4a6b1c9620fd156c87d957380e54ea
Chupin Clément <clement.chupin@inrae.fr>
2023-12-01 15:40:27 +0100

    add speed version, proof of concept

--- src/ros_topic_utils.py
+++ src/ros_topic_utils.py
@@ -12,74 +12,74 @@ import os
 from visualization_msgs.msg import Marker
 from visualization_msgs.msg import MarkerArray
 from geometry_msgs.msg import Point
-
+from std_msgs.msg import ColorRGBA
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
 def point_cloud_4d(points, parent_frame):
-    """ Creates a point cloud message.
-    Args:
-        points: Nx4 array of xyz positions (m) and intensity (0..1)
-        parent_frame: frame in which the point cloud is defined
-    Returns:
-        sensor_msgs/PointCloud2 message
-    """
-    points = np.array(points.cpu())
-    ros_dtype = sensor_msgs.PointField.FLOAT32
-    dtype = np.float32
-    itemsize = np.dtype(dtype).itemsize
+	""" Creates a point cloud message.
+	Args:
+		points: Nx4 array of xyz positions (m) and intensity (0..1)
+		parent_frame: frame in which the point cloud is defined
+	Returns:
+		sensor_msgs/PointCloud2 message
+	"""
+	points = np.array(points.cpu())
+	ros_dtype = sensor_msgs.PointField.FLOAT32
+	dtype = np.float32
+	itemsize = np.dtype(dtype).itemsize
 
-    data = points.astype(dtype).tobytes()
+	data = points.astype(dtype).tobytes()
 
-    fields = [sensor_msgs.PointField(
-        name=n, offset=i*itemsize, datatype=ros_dtype, count=1)
-        for i, n in enumerate(["x", "y", "z","intensity"])]
+	fields = [sensor_msgs.PointField(
+		name=n, offset=i*itemsize, datatype=ros_dtype, count=1)
+		for i, n in enumerate(["x", "y", "z","intensity"])]
 
-    header = std_msgs.Header(frame_id=parent_frame, stamp=rospy.Time.now())
+	header = std_msgs.Header(frame_id=parent_frame, stamp=rospy.Time.now())
 
-    return sensor_msgs.PointCloud2(
-        header=header,
-        height=1,
-        width=points.shape[0],
-        is_dense=False,
-        is_bigendian=False,
-        fields=fields,
-        point_step=(itemsize * 4),
-        row_step=(itemsize * 4 * points.shape[0]),
-        data=data
-    )
+	return sensor_msgs.PointCloud2(
+		header=header,
+		height=1,
+		width=points.shape[0],
+		is_dense=False,
+		is_bigendian=False,
+		fields=fields,
+		point_step=(itemsize * 4),
+		row_step=(itemsize * 4 * points.shape[0]),
+		data=data
+	)
 def point_cloud_3d(points, parent_frame):
-    """ Creates a point cloud message.
-    Args:
-        points: Nx3 array of xyz positions (m)
-        parent_frame: frame in which the point cloud is defined
-    Returns:
-        sensor_msgs/PointCloud2 message
-    """
-    points = np.array(points)
-    ros_dtype = sensor_msgs.PointField.FLOAT32
-    dtype = np.float32
-    itemsize = np.dtype(dtype).itemsize
+	""" Creates a point cloud message.
+	Args:
+		points: Nx3 array of xyz positions (m)
+		parent_frame: frame in which the point cloud is defined
+	Returns:
+		sensor_msgs/PointCloud2 message
+	"""
+	points = np.array(points)
+	ros_dtype = sensor_msgs.PointField.FLOAT32
+	dtype = np.float32
+	itemsize = np.dtype(dtype).itemsize
 
-    data = points.astype(dtype).tobytes()
+	data = points.astype(dtype).tobytes()
 
-    fields = [sensor_msgs.PointField(
-        name=n, offset=i*itemsize, datatype=ros_dtype, count=1)
-        for i, n in enumerate(["x", "y", "z"])]
+	fields = [sensor_msgs.PointField(
+		name=n, offset=i*itemsize, datatype=ros_dtype, count=1)
+		for i, n in enumerate(["x", "y", "z"])]
 
-    header = std_msgs.Header(frame_id=parent_frame, stamp=rospy.Time.now())
+	header = std_msgs.Header(frame_id=parent_frame, stamp=rospy.Time.now())
 
-    return sensor_msgs.PointCloud2(
-        header=header,
-        height=1,
-        width=points.shape[0],
-        is_dense=False,
-        is_bigendian=False,
-        fields=fields,
-        point_step=(itemsize * 3),
-        row_step=(itemsize * 3 * points.shape[0]),
-        data=data
-    )
+	return sensor_msgs.PointCloud2(
+		header=header,
+		height=1,
+		width=points.shape[0],
+		is_dense=False,
+		is_bigendian=False,
+		fields=fields,
+		point_step=(itemsize * 3),
+		row_step=(itemsize * 3 * points.shape[0]),
+		data=data
+	)
 
 
 class ros_topic_manager:
@@ -99,11 +99,13 @@ class ros_topic_manager:
 def get_line(a,b,marker_link="base_link"):
 	marker = Marker()#zed2i_left_camera_frame
 	marker.header.frame_id = marker_link
-	marker.type = marker.LINE_STRIP
+	# marker.type = marker.LINE_STRIP
+	marker.type = 4
 	marker.action = marker.ADD
+	marker.ns ="toto"
 
 
-	scale_reduce = 0.02
+	scale_reduce = 0.8
 	# marker scale
 	marker.scale.x = scale_reduce
 	marker.scale.y = scale_reduce
@@ -111,7 +113,7 @@ def get_line(a,b,marker_link="base_link"):
 
 	# marker color
 	marker.color.a = 1.0
-	marker.color.r = 0.0
+	marker.color.r = 1.0
 	marker.color.g = 0.0
 	marker.color.b = 0.0
 
@@ -122,24 +124,26 @@ def get_line(a,b,marker_link="base_link"):
 	marker.pose.orientation.w = 1.0
 
 	# marker position
-	marker.pose.position.x = 0.0
-	marker.pose.position.y = 0.0
-	marker.pose.position.z = 0.0
+	marker.pose.position.x = a[0].item()
+	marker.pose.position.y = a[1].item()
+	marker.pose.position.z = a[2].item()
+	
 
 	# marker line points
 	marker.points = []
 	# first point
 	first_line_point = Point()
-	first_line_point.x = a[0]
-	first_line_point.y = a[1]
-	first_line_point.z = a[2]
-	marker.points.append(first_line_point)
+	first_line_point.x = a[0].item()
+	first_line_point.y = a[1].item()
+	first_line_point.z = a[2].item()
+
 	# second point
 	second_line_point = Point()
-	second_line_point.x = b[0]
-	second_line_point.y = b[1]
-	second_line_point.z = b[2]
-	marker.points.append(second_line_point)
+	second_line_point.x = b[0].item()
+	second_line_point.y = b[1].item()
+	second_line_point.z = b[2].item()
+
+	marker.points = [first_line_point,second_line_point]
 
 	return marker
 
@@ -150,6 +154,7 @@ def publish_lines(list_A,list_B,marker_link="base_link",marker_array_pub=rospy.P
 		msg.markers.append(get_line(list_A[idx],list_B[idx],marker_link))
 		msg.markers[-1].id = id
 		id += 1
+	# print(msg)
 	marker_array_pub.publish(msg)
 
 # def send_all_markers(self,publisher):
